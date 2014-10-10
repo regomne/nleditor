fa925712433930a1138cddbed1ef50784da69032
Regomne <fallingsunz@gmail.com>
2014-10-10 23:54:24 +0800

    adding

--- backend.js
+++ backend.js
@@ -1,6 +1,8 @@
 var fs=require('fs');
 var querystring=require('querystring');
+var iconv=require('iconv-lite');
 //var uiproc=require('./uiproc');
+//var configs=require('./configs')
 
 function recvCmd(qs,data)
 {
@@ -36,16 +38,84 @@ function recvCmd(qs,data)
 	return ret;
 }
 
+/*
+config:
+{
+	colors:
+	{
+		
+	}
+}
+*/
+
+/*
+ *project:
+ *{
+ *	linesOri:[...]
+ *	linesMod:{1:XX,3:XX}
+ *	linesMark:{}
+ *	codecOfOri:'xxx'
+ *	codecOfNew:'xxx'
+ *	
+ *}
+ */
+
+function createProject(cmd,data)
+{
+	var oriFileName=cmd.oriFileName;
+	if(oriFileName===undefined)
+		throw "must have oriFileName";
+	if(typeof(oriFileName)!='string')
+		throw "oriFileName must be unique";
+
+	var codec=cmd.codec;
+	if(codec && !iconv.encodingExists(codec))
+		throw "unknown codec";
+
+	fs.readFile(oriFileName,function(err,data)
+	{
+		if(err) throw err;
+		
+		var proj=readLinesOfFile;
+		
+		var ff=iconv.encode(JSON.stringify(proj),'utf16le');
+		
+	});
+}
+
 function openProject(cmd)
 {
-	var fname=cmd['name'];
+	var fname=cmd.name;
 	if(fname===undefined)
 		throw "no proj name in openProject";
 	if(typeof(fname)!='string')
 		throw "proj name must be unique";
 	fs.readFile(fname+'.proj',function(err,data){
 		if(err) throw err;
-		var proj=JSON.parse(data);
+		var proj=JSON.parse(data.toString('utf16le'));
+		uiproc.addingProject(proj);
 	});
 	return 'reading';
+}
+
+function readLinesOfFile(data,codec)
+{
+	if(codec===undefined)
+	{
+		if(data[0]=='\xfe' && data[1]=='\xff')
+			codec='utf16le';
+		else if(data[0]=='\xff' && data[1]=='\xfe')
+			codec='utf16be';
+		else if(data[0]=='\xef' && data[1]=='\xbb' && data[2]=='\xbf')
+			codec='utf8';
+		else
+			codec=configs.defaultCodec;
+	}
+
+	var str=iconv.decode(data,codec);
+	var lines=str.split('\r\n');
+	return {
+		linesOri:lines,
+		codecOri:codec,
+	};
 }
